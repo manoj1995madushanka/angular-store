e1170a95d293b4bc9cf766e3923d9381519562cc
manoj1995madushanka <madushankamanoj414@gmail.com>
2022-11-20 18:52:01 +0530

    pipes

--- src/app/spectacle/spectacle.component.ts
+++ src/app/spectacle/spectacle.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit } from '@angular/core';
 interface Spectacle{
   brand: string;
   model: string;
-  image: string
+  image: string;
+  price: number;
 }
 
 @Component({
@@ -17,17 +18,20 @@ export class SpectacleComponent implements OnInit {
     {
       image:"https://www.spexbay.com/media/catalog/product/cache/e4d64343b1bc593f1c5348fe05efa4a6/s/p/spexbay_fp1972_-_c1.jpg",
       brand:"SPEXBAY",
-      model:"FP1972 - C1"
+      model:"FP1972 - C1",
+      price: 7300
     },
     {
       image:"https://www.spexbay.com/media/catalog/product/cache/e4d64343b1bc593f1c5348fe05efa4a6/l/a/lah025-c3_01.jpg",
       brand:"SPEXBAY",
-      model:"LAH025 C3"
+      model:"LAH025 C3",
+      price: 10300
     },
     {
       image:"https://www.spexbay.com/media/catalog/product/cache/e4d64343b1bc593f1c5348fe05efa4a6/a/3/a3004c-c2_01.jpg",
       brand:"SPEXBAY",
-      model:"A3004C C2"
+      model:"A3004C C2",
+      price: 20000
     }
   ]
 
