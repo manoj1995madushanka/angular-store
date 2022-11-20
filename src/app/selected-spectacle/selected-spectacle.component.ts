e600cdc181677fe55371a27d7d1b2c8e2f6a8b07
manoj1995madushanka <madushankamanoj414@gmail.com>
2022-11-20 21:20:42 +0530

    remove unwanted code

--- src/app/selected-spectacle/selected-spectacle.component.ts
+++ src/app/selected-spectacle/selected-spectacle.component.ts
@@ -6,23 +6,14 @@ import {Spectacle} from "../types/Spectacle";
   templateUrl: './selected-spectacle.component.html',
   styleUrls: ['./selected-spectacle.component.css']
 })
-export class SelectedSpectacleComponent implements OnInit,OnDestroy {
+export class SelectedSpectacleComponent implements OnInit {
 
   @Input() spectacle: Spectacle= {} as Spectacle; // create empty object
   @Output() emitSpectacle = new EventEmitter<Spectacle>();
 
-  myInterval:any=null;
-
   constructor() { }
 
   ngOnInit(): void {
-    this.myInterval = setInterval(()=>{
-      console.log("Hi")
-    },1000)
-  }
-
-  ngOnDestroy(): void {
-   clearInterval(this.myInterval);
   }
 
   addToCart() {
