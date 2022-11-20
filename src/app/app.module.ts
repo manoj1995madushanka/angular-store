4c88c1e22c2f004b336e852e165dbbd6a9770b6d
manoj1995madushanka <madushankamanoj414@gmail.com>
2022-11-20 14:38:12 +0530

    create glass component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GlassComponent } from './glass/glass.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GlassComponent
   ],
   imports: [
     BrowserModule,
