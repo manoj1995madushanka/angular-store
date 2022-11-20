9dedcbf7aa3f81a9af459627e7e3ae0af92f0b33
manoj1995madushanka <madushankamanoj414@gmail.com>
2022-11-20 21:38:53 +0530

    get spectacles from the service

---
+++ src/app/spectacle/spectacles.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import {Spectacle} from "../types/Spectacle";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SpectaclesService {
+
+  spectacles: Spectacle[] = [
+    {
+      image:"https://www.spexbay.com/media/catalog/product/cache/e4d64343b1bc593f1c5348fe05efa4a6/s/p/spexbay_fp1972_-_c1.jpg",
+      brand:"SPEXBAY",
+      model:"FP1972 - C1",
+      price: 7300
+    },
+    {
+      image:"https://www.spexbay.com/media/catalog/product/cache/e4d64343b1bc593f1c5348fe05efa4a6/l/a/lah025-c3_01.jpg",
+      brand:"SPEXBAY",
+      model:"LAH025 C3",
+      price: 10300
+    },
+    {
+      image:"https://www.spexbay.com/media/catalog/product/cache/e4d64343b1bc593f1c5348fe05efa4a6/a/3/a3004c-c2_01.jpg",
+      brand:"SPEXBAY",
+      model:"A3004C C2",
+      price: 20000
+    }
+  ];
+
+  constructor() { }
+
+  getSpectacles() {
+    return this.spectacles;
+  }
+}
